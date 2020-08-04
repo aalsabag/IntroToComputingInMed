83dc32adede2b47e35a07f6bdf0636761b05b38a
aalsabag <ahmed.alsabag@gmail.com>
2020-08-03 23:57:40 -0500

    adding variable project

--- Lesson3-Variables/lesson.java
+++ Lesson3-Variables/lesson.java
@@ -34,13 +34,47 @@ class Lesson {
         System.out.println(c * d); //12
 
         // We can also do division, but there is an important rule to remember
-        // Operating on two ints will yield and int
+        // Operating on two ints will yield an int
         // Operating on an int and a double will yield a double!
         /* **********PRACTICE POINT*********** */
 
         System.out.println(d / c); // = 1 not 1.333
 
+        // Another operation is the modulus operator denoted by %
+        // It will give the remainder after performing a division
 
+        System.out.println(d % c); // = 1   3 goes into 4 one time with a remainder 1
+        System.out.println(18 % 4); // = 2 ie. 4R2
+
+        System.out.println(5 % 15); // = 5 0R5
+
+        // Don't forget PEMDAS! This is still math afterall
+        System.out.println(6 + (18 % (17 - 12))); 
+        System.out.println(203 % 100 / 5);
+        
+        // Mixing types in PEMDAS
+        System.out.println(7 / 3 * 1.2 + 3 / 2); //Tough one!
+
+        // These operators can be used with other types like strings!
+        // String concatenation is using a + between a string and another
+        // value to make a longer string
+        System.out.println("ab" + "cd"); // abcd
+        System.out.println("ab" + 13); // ab13
+        // PEMDAS stilllll applies
+        System.out.println(5.0 / 2 + "abcd"); // "2.5abcd"
+
+        // Putting it all together
+        String name = "John";
+        double income = 20000;
+        double tax = .2;
+        int bills = 10000;
+
+        System.out.println("My name is: " + name);
+        income = income * tax; // notice that we don't have to specify a type
+        System.out.println(name + "'s income is: " + income);
+        double takeHome = income - bills;
+        System.out.println(name + "'s take home income is: " + takeHome);
+        
     }
     
 }
---
+++ Lesson3-Variables/project.java
@@ -0,0 +1,36 @@
+class Project {
+    public static void main(String args[]) {
+        /*
+        There are a lot of calculations those in medicine might need to perform.
+        A lot of them you may have heard of in some shape or form. Today we will
+        calculate O2 saturation, BMI, BMI prime, Creatine Clearance, and the
+        Sodium correction for hyperglycemia
+
+        1. Each of these calculations must be performed according to the specifications
+        below.
+        2. Write each calculation in its own separate static method.
+        
+        The methods will be:
+        calculateO2() 
+
+        calculateBMI()
+
+        calculateBMIPrime()
+
+        calculateCreatineClearance() - given 3 values of age in years, weight in lbs,
+        and creatine concentration in mg/dL calculate the creatine clearance to assess 
+        a patients renal function.
+        Cockcroft-Gault CrCl, mL/min = (140 – age) × (weight, kg) × (0.85 if female) / (72 × Cr)
+        OUTPUT: The creatine clearance rate for a 30 year old 150 pound male with a creatine
+        concentration of .7 mg/dL is 148 mL/min
+
+        calculateSodiumCorrection() - given 2 values of sodium (mEq/L) and Glucose
+        (mg/dL) calculate the corrected sodium concentration. 
+        Corrected Sodium (Hillier, 1999) = Measured sodium + 0.024 * (Serum glucose - 100)
+        OUTPUT: The corrected sodium concentration in hyperglycemia for an original sodium
+        concentration of 137 mEq/L and 70 mg/dL is 136 mEq/L
+        https://pubmed.ncbi.nlm.nih.gov/10225241/
+        */
+        
+    }
+}
