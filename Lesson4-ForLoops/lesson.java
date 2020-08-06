ce80bd3e851a2ac0735c38bcca5e1839c4e1e714
aalsabag <ahmed.alsabag@gmail.com>
2020-08-05 23:54:38 -0500

    initial loop lesson

---
+++ Lesson4-ForLoops/lesson.java
@@ -0,0 +1,17 @@
+class Lesson {
+
+    public static void main (String args[]){
+        System.out.println("Hello World!!");
+
+        // A java for loop will repeat a particular task multiple time
+        // What do you think this line does
+        for (int i=0; i<5; i++){
+            System.out.println("hello");
+        }
+        // It prints hello 5 times!!
+
+        
+        
+    }
+    
+}
