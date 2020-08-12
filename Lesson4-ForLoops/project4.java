0c94e80423920196925f8961d0fe4675a033d8f7
aalsabag <ahmed.alsabag@gmail.com>
2020-08-11 23:12:52 -0500

    adding project

--- Lesson4-ForLoops/project4.java
+++ Lesson4-ForLoops/project4.java
@@ -1,7 +1,43 @@
 class Project4 {
     public static void main(String args[]) {
         /*
-        
+        Due to COVID-19 a hospital is attempting to apply social distancing
+        for hospital beds. The hospital has mandated that there be at least a bed 
+        between each patient. Occupation of beds is represented by a grid like this.
+        *#*#*
+        #*#*#
+        *#***
+        A * represents an unoccupied bed and a # represents an occupied bed.
+
+        The above is a valid orientation.
+
+        You have two jobs. Create both hospital orientations listed below, each in
+        their own separate methods, evenBed() and oddBed() respectively.
+
+        *#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#
+
+
+        #*#*#*#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#*#*#*
+        #*#*#*#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#*#*#*
+        #*#*#*#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#*#*#*
+        #*#*#*#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#*#*#*
+        #*#*#*#*#*#*#*#*#*#*#*#
+        *#*#*#*#*#*#*#*#*#*#*#*
+        #*#*#*#*#*#*#*#*#*#*#*#
+
+        Remember to use for loops and the modulus operator to perform both of
+        these tasks! You will find them very useful
+
+        You may NOT just print these out directly using System.out.println()
         */
     }
 
