5b6d29d9f3cc0beee648ed8192b6fef687d856b3
aalsabag <ahmed.alsabag@gmail.com>
2020-08-18 01:57:26 -0500

    added full project 7

--- Lesson7-ConditionalsIntro/lesson7.java
+++ Lesson7-ConditionalsIntro/lesson7.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class Lesson7 {
     public static void main(String args[]){
         // Scanner - allows us to read inputs from different sources
@@ -6,5 +7,24 @@ class Lesson7 {
 
         // The scanner we will be using comes from java.util.Scanner so you will have
         // to import it at the top of this file
+
+        // The scanner has a few methods, most notable are nextInt(), nextDouble(), nextLine()
+        // Each of these methods will wait until a user presses the Enter key
+
+        // First we instantiate the Scanner, we'll call it console
+        Scanner console = new Scanner(System.in);
+
+        System.out.print("What day is it today? ");
+        int day = console.nextLine(); // It will wait until you type something and press enter. It then stores in the day variable
+        System.out.println("Today is " + day); // This will print out the day variable
+       
+        
+        System.out.print("What is your age ");
+        int age = console.nextInt();
+        System.out.println("You are " + age + " years old");
+        // console.close(); // Once you are done with a console it is important to close it for proper clean up. There is a better way, but we will cover that at a later time.
+
+
+        
     }
 }
--- Lesson7-ConditionalsIntro/project7.java
+++ Lesson7-ConditionalsIntro/project7.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 class Project7 {
     public static void main(String args[]){
+        /*
+        The first thing you do at a doctor's office upon your arrival is check in. Usually, 
+        you are handed a form and a pen to fill out information.
+        As part of an upgrade, a hospital wants to upgrade this process by using tablets.
+        Your task is to prompt the user to enter answers to a series of questions. At the end of
+        all of the questions, you will display a report of the answers.
 
+        Here are the questions:
+
+        1. What is your full name?
+        2. What is your age?
+        3. What is your gender? Enter either M (male), F (female), N (N/A): 
+        4. What is your weight in lbs (round to the nearest pound)?
+        5. What is your height in inches (round to the nearest inch)?
+        6. Who is your insurance provider?
+        7. What is your state of residence?
+        8. How would you describe your condition in a sinlge sentence?
+
+        Upon completion of the questions, a report must be displayed with these fields in this order:
+        Name - full name
+        Age - age
+        Age classification - 'Child' if under 18. "Adult" if 18 or older
+        Gender - 'Male', 'Female', 'N/A'
+        Weight - Weight in lbs (display the units(lbs))
+        Height - Height in inches (display the units (inches))
+        BMI - BMI calculation
+        Weight classification - 'Underweight' if less than 18.5, 'Normal' 18.5-24.9, 'Overweight' 25-29.9, 30-40 'Obese', "Morbidly Obese" for anything above that
+        Insurance provider - Display the insurance provider if it is 'Bluecross' or 'Kaiser', for any other values display "Uknown"
+        State - Display state
+        Insurance accepted - If they are bluecross or kaiser and they don't reside in Wyoming, Alaska, Hawaii, display 'yes' otherwise display 'no'
+        Condition - Display condition
+
+        Here is an example output:
+        
+        Name - Steve Jacobs
+        Age - 57
+        Age Classification - Adult
+        Gender - Male
+        Weight - 160 lbs
+        Height - 69 inches
+        BMI - 23.6
+        Weight Classification - Normal
+        Insurance Provider - Kaiser
+        State - California
+        Insurance accepted: yes
+        Condition: Mild Headaches
+        */
     }
 }
