6a47e4e28d2d0e26462da58a35a3b3f98b71d6db
aalsabag <ahmed.alsabag@gmail.com>
2020-08-17 23:58:48 -0500

    add scanner

--- Lesson7-ConditionalsIntro/lesson7.java
+++ Lesson7-ConditionalsIntro/lesson7.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 class Lesson7 {
     public static void main(String args[]){
-        // Conditionals are a means for making decisions
+        // Scanner - allows us to read inputs from different sources
+        // Today we'll cover how to read values from the command line.
+
+        // The scanner we will be using comes from java.util.Scanner so you will have
+        // to import it at the top of this file
     }
 }
